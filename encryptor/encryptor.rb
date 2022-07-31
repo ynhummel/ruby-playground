898ff4c3195bf37e7450c61efcc7ee1f29fefc33
ynhummel <yurihummel@outlook.com>
2022-07-31 01:03:50 -0300

    refactor: chance Encryptor methods into class methods

--- encryptor/encryptor.rb
+++ encryptor/encryptor.rb
@@ -24,18 +24,18 @@ class Encryptor
       string.split('').map { |letter| decrypt_table[letter] }.join('')
     end
 
-    def encrypt_file(filename)
+    def encrypt_file(filename, rot)
       file = File.open(filename, 'r')
       input = file.read
-      encrypted_input = encrypt(input)
+      encrypted_input = encrypt(input, rot)
       encrypted_file = File.open("#{filename}.encrypted", 'w')
       encrypted_file.write(encrypted_input)
     end
 
-    def decrypt_file(filename)
+    def decrypt_file(filename, rot)
       file = File.open(filename, 'r')
       input = file.read
-      decrypted_input = decrypt(input)
+      decrypted_input = decrypt(input, rot)
       decrypted_file = File.open("#{filename}.decrypted", 'w')
       decrypted_file.write(decrypted_input)
     end
