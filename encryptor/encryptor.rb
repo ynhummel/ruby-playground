f3304040afb3c90a08441360719a3750e05b8a6d
ynhummel <yurihummel@outlook.com>
2022-07-31 00:59:39 -0300

    refactor: Convert Encryptor methods into class methods

--- encryptor/encryptor.rb
+++ encryptor/encryptor.rb
@@ -1,45 +1,43 @@
 class Encryptor
-  attr_reader :rot
+  class << self
+    def encrypt_table
+      cipher_table(rot)
+    end
 
-  def initialize(rotation)
-    @rot = rotation
-  end
+    def decrypt_table
+      cipher_table(-rot)
+    end
 
-  def encrypt_table
-    cipher_table(rot)
-  end
+    def cipher(rot)
+      chars = (' '..'z').to_a
+      rotated_chars = chars.rotate(rot)
+      Hash[chars.zip(rotated_chars)]
+    end
 
-  def decrypt_table
-    cipher_table(-rot)
-  end
+    def encrypt(string, rot)
+      encrypt_table = cipher(rot)
+      string.split('').map { |letter| encrypt_table[letter] }.join('')
+    end
 
-  def cipher_table(rot)
-    chars = (' '..'z').to_a
-    rotated_chars = chars.rotate(rot)
-    Hash[chars.zip(rotated_chars)]
-  end
+    def decrypt(string, rot)
+      decrypt_table = cipher(-rot)
+      string.split('').map { |letter| decrypt_table[letter] }.join('')
+    end
 
-  def encrypt(string)
-    string.split('').map { |letter| encrypt_table[letter] }.join('')
-  end
+    def encrypt_file(filename)
+      file = File.open(filename, 'r')
+      input = file.read
+      encrypted_input = encrypt(input)
+      encrypted_file = File.open("#{filename}.encrypted", 'w')
+      encrypted_file.write(encrypted_input)
+    end
 
-  def decrypt(string)
-    string.split('').map { |letter| decrypt_table[letter] }.join('')
-  end
-
-  def encrypt_file(filename)
-    file = File.open(filename, 'r')
-    input = file.read
-    encrypted_input = encrypt(input)
-    encrypted_file = File.open("#{filename}.encrypted", 'w')
-    encrypted_file.write(encrypted_input)
-  end
-
-  def decrypt_file(filename)
-    file = File.open(filename, 'r')
-    input = file.read
-    decrypted_input = decrypt(input)
-    decrypted_file = File.open("#{filename}.decrypted", 'w')
-    decrypted_file.write(decrypted_input)
+    def decrypt_file(filename)
+      file = File.open(filename, 'r')
+      input = file.read
+      decrypted_input = decrypt(input)
+      decrypted_file = File.open("#{filename}.decrypted", 'w')
+      decrypted_file.write(decrypted_input)
+    end
   end
 end
