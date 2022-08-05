28feae1d79a9da23e0ae2dee4ffe996665ad9285
Yuri Hummel <yuri.hummel@tray.net.br>
2022-08-05 00:14:22 -0300

    add more tests and refactor cli_app

--- encryptor/cli_app.rb
+++ encryptor/cli_app.rb
@@ -2,7 +2,7 @@ require_relative './encryptor'
 require 'digest'
 
 def wrong_pass
-  puts 'Wrong password, exiting program!'
+  puts 'Senha incorreta, finalizando programa'
   exit
 end
 
@@ -11,49 +11,50 @@ pass = gets.chomp
 hashed_pass = Digest::SHA256.hexdigest(pass)
 wrong_pass if hashed_pass != "d0c278cfad79b02c45450c3e88e4206ee6e046c30655306c045c5c056676cf07"
 
-puts 'Escolha a opção:'
+puts
 puts '1 - Encrypt'
 puts '2 - Cycle Encrypt'
 puts '3 - Decrypt'
 puts '4 - Cycle Decrypt'
 puts '5 - Sair'
+puts 'Escolha a opção: '
 
 def encrypt
-  system('clear')
-  puts 'Digite a mensagem a ser criptografada'
+  puts 'Digite a mensagem a ser criptografada:'
   msg = gets.chomp
 
   puts 'Número de rotações: '
   rot = gets.chomp
 
   puts "#{Encryptor.encrypt(msg, rot.to_i)}".inspect
+  exit
 end
 
 def decrypt
-  system('clear')
-  puts 'Digite a mensagem a ser descriptografada'
+  puts 'Digite a mensagem a ser descriptografada:'
   msg = gets.chomp
 
   puts 'Número de rotações: '
   rot = gets.chomp
 
   puts "#{Encryptor.decrypt(msg, rot.to_i)}".inspect
+  exit
 end
 
 def cycle_encrypt
-  system('clear')
-  puts 'Digite a mensagem a ser criptografada'
+  puts 'Digite a mensagem a ser criptografada:'
   msg = gets.chomp
 
   puts "#{Encryptor.cycle_encrypt(msg)}".inspect
+  exit
 end
 
 def cycle_decrypt
-  system('clear')
-  puts 'Digite a mensagem a ser descriptografada'
+  puts 'Digite a mensagem a ser descriptografada:'
   msg = gets.chomp
 
   puts "#{Encryptor.cycle_decrypt(msg)}".inspect
+  exit
 end
 
 option = 0
--- encryptor/test/encryptor_test.rb
+++ encryptor/test/encryptor_test.rb
@@ -3,10 +3,30 @@ require_relative "../encryptor.rb"
 
 class EncryptorTest < Test::Unit::TestCase
   def test_encrypt
-    assert_equal(Encryptor.encrypt("Hello World!", 11), "Spwwz+bz\"wo,")
+    input = Encryptor.encrypt("Hello World!", 11)
+    output = "Spwwz+bz\"wo,"
+
+    assert_equal(input, output)
   end
 
   def test_decrypt
-    assert_equal(Encryptor.decrypt("Spwwz+bz\"wo,", 11), "Hello World!")
+    input = Encryptor.decrypt("Spwwz+bz\"wo,", 11)
+    output = "Hello World!"
+
+    assert_equal(input, output)
+  end
+
+  def test_cycle_encrypt
+    input = Encryptor.cycle_encrypt("Hello World!")
+    output = "U 3y*Bd*9yzC"
+
+    assert_equal(input, output)
+  end
+
+  def test_cycle_decrypt
+    input = Encryptor.cycle_decrypt("U 3y*Bd*9yzC")
+    output = "Hello World!"
+
+    assert_equal(input, output)
   end
 end
